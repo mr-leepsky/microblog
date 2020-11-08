6f280900d30bd7be19accb5f98cdaec2a3575f63
Marcin Lipa <marcin.lipa@protonmail.com>
2020-11-08 19:49:03 +0100

    Chapter 4: Database

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from config import Config
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes  # noqa: E402, F401
+from app import models, routes  # noqa: E402, F401
